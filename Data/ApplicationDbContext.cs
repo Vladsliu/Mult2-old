1870e34651faa5968e4df6320561ced4f3ade392
Vladsliu <vlad.sliusarch@gmail.com>
2023-02-04 13:15:07 +0100

    Mult2
    Mult2

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mult2.Models;
+
+namespace Mult2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mult2.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public DateTime CreateDateTime { get; set;} = DateTime.Now;
+    }
+}
