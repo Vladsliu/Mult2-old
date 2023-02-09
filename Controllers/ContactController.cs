4e916a03ba8d807b3227340ab69fca96ebc0a9ab
Vladsliu <vlad.sliusarch@gmail.com>
2023-02-09 18:15:43 +0100

    Mult2

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mult2.Data;
+using Mult2.Models;
+
+namespace Mult2.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ContactController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+            return View("Index");//??
+            }
+            _context.Add(contact);
+            _context.SaveChanges();
+            return RedirectToAction("Create");//?
+        }
+    }
+
+   
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mult2.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Names { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
